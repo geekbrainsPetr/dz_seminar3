9bc08678136446b39e0373a14fb444e4e6f8b046
Petr <petr_mail@mail.ru>
2022-10-23 00:29:57 +0300

    два задания

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿
+// вход число N и выдаёт таблицу кубов чисел от 1 до N
+/*
+void K( int num)
+
+{
+    int current = 1;
+    while (current <= num)
+    {
+    Console.Write($"{current * current * current} , ");
+    current ++; 
+    }
+}
+
+Console.WriteLine("Введите  число");
+int num = Convert.ToInt32(Console.ReadLine());
+K( num);
+// всё как в задаче. В строкУ, через запятую.
+
+*/
+
+// проверка пятизначного числа на палендром
+
+/*
+
+Console.WriteLine("Введите пятизначное число");
+String num = Console.ReadLine();
+int length = num.Length;
+
+if (length ==5)
+{
+        
+        
+        if (num[0] == num[4] && num[1]==num[3])
+        {
+        Console.WriteLine($" Число {num}  палендром");
+        }
+        else
+        {
+        Console.WriteLine($" Число {num} не палендром");
+        }
+}    
+else
+        {
+        Console.WriteLine($"Число {num} не является пятизначным"); 
+        }
+
+*/
+
+
