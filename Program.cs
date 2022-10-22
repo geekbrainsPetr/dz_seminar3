df33313cde5b5e20ff7ab434a661b30a76fc7b1a
Petr <petr_mail@mail.ru>
2022-10-23 00:51:33 +0300

    три задания

--- Program.cs
+++ Program.cs
@@ -47,4 +47,30 @@ else
 
 */
 
+// Расстояние в 3D
 
+
+
+double FindWay(double x1, double y1, double z1, double x2, double  y2, double z2)
+{
+   
+    double Way = Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2) + Math.Pow(z2-z1, 2));
+    return Way;
+}
+
+Console.WriteLine("введите координату x1");
+double x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите координату y1");
+double y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите координату z1");
+double z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введиту координату x2");
+double x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите координату y2");
+double y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите координату z2");
+double z2 = Convert.ToInt32(Console.ReadLine());
+
+
+double res = FindWay(x1, y1, z1, x2, y2, z2);
+Console.WriteLine($"расстояние между точек {res}");
